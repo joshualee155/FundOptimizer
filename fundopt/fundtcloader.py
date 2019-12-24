a2aeff87a52ca06b13bb56cd12862985ecb765a9
Joshua Lee <hpcq_124@163.com>
2019-12-24 13:34:41 +0000

    bug fix: transaction cost

--- fundopt/fundtcloader.py
+++ fundopt/fundtcloader.py
@@ -1,7 +1,6 @@
 import os.path
-import numpy as np
+import pandas as pd
 import cvxpy as cvx
-from collections import defaultdict
 
 DEFAULT_BUYCOST  = 0.12
 DEFAULT_SELLCOST = 0.5
@@ -13,22 +12,25 @@ class FundTransactionCostLoader(object):
 
 
     def __init__(self, prefix = 'refData'):
-        fundCodes, buyCosts, sellCosts = [np.array(_) for _ in zip(*np.genfromtxt( os.path.join(prefix, 'TxnCost.txt'),
-                                                        unpack=True,
-                                                        dtype=[('code', 'S6'), ('buyRates', 'f8'), ('sellRates', 'f8')]
-                                                        ))]
-        buyCosts[np.isnan(buyCosts)]   = DEFAULT_BUYCOST
-        sellCosts[np.isnan(sellCosts)] = DEFAULT_SELLCOST
+        
+        df = pd.read_csv( os.path.join( prefix, 'TxnCost.txt' ), sep = ' ', header = None, names = ['Code', 'BuyCost', 'SellCost'] )
+        
+        df[ 'Code' ] = df['Code'].astype(str).str.zfill(6)
+
         #print buyCosts, sellCosts
-        self._txnCosts = defaultdict( lambda : defaultTxnCost )
-        for fundCode, buyCost, sellCost in zip( fundCodes, buyCosts, sellCosts ):
-            self._txnCosts[fundCode] = lambda _x : cvx.scalene(_x, buyCost/NORMAL_FACTOR, sellCost/NORMAL_FACTOR)
+        self._txnCosts = dict()
+        for fundCode, buyCost, sellCost in df.itertuples( index = False ):
+            self._txnCosts[fundCode] = lambda _x, b=buyCost, s=sellCost : cvx.scalene(_x, b/NORMAL_FACTOR, s/NORMAL_FACTOR)
 
     def getTxnCost(self, fundCode):
-        return self._txnCosts[fundCode]
+        cost = self._txnCosts.get( fundCode )
+        if cost is None:
+            return defaultTxnCost
+        else:
+            return cost
 
 if __name__ == "__main__":
     txnCostLoader = FundTransactionCostLoader()
-    fundcode  = '001550'
+    fundcode  = '000198'
     x = cvx.Variable()
     print( (txnCostLoader.getTxnCost(fundcode)(x)) <= 0 )
